9589b5fc12784e82d37ace95c5ab5e02f10c10b0
Vicctory <123509837+viccdevz@users.noreply.github.com>
2023-03-14 21:01:13 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,6 +3,6 @@ Config.Ban = false -- Ban sistemi kapalımı açık olsun ayarı (BAN = TRUE / K
 Config.BanAyar = 1 -- Oyuncu belirlediğiniz sayıdan daha fazla öldürürse sunucudan yasaklanacaktır (Önerilen 2)
 Config.KickBanMesaj = "Magic bullet tespit edildi -by viccdevz." -- Kick veya Banlandığında gelen mesaj.
 Config.WebHookGonder = true
-Config.WebhookURL = "https://discord.com/api/webhooks/1083802447451004948/JfXmXE6jRkZ-TVpZ3UI0nCRN_0hb9-QQff0fTT_XMFzQKtYqtkKpbt7v6AcabJwf2EvR"
+Config.WebhookURL = "webhooklinki"
 Config.BanAyar = function(source)
 end
